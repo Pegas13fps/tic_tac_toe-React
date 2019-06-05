9eeb29cc7d01a8863b7f389baa2cc16ea309659d
Pegas13fps <Tymoschenkob@gmail.com>
2019-06-06 00:54:57 +0300

    Updates

---
+++ precache-manifest.968411dac7e25d1b3f44e93e36b51b35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bbdb3df9b24d97d0f44607b500fa677",
+    "url": "/tic_tac_toe-React/index.html"
+  },
+  {
+    "revision": "494ddc91c2bf678b0bd6",
+    "url": "/tic_tac_toe-React/static/css/main.e694da13.chunk.css"
+  },
+  {
+    "revision": "1439f38f9ece51aaf058",
+    "url": "/tic_tac_toe-React/static/js/2.fde396be.chunk.js"
+  },
+  {
+    "revision": "494ddc91c2bf678b0bd6",
+    "url": "/tic_tac_toe-React/static/js/main.921baa5d.chunk.js"
+  },
+  {
+    "revision": "46a5b911523ed1755a67",
+    "url": "/tic_tac_toe-React/static/js/runtime~main.25341581.js"
+  }
+]);
